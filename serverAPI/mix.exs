ec652994f71452283150fcf15e599f45218f3f4e
Rafael Passos <rafa_auyer@icloud.com>
2017-09-29 16:03:50 -0300

    Migrated to sqlite_ecto2

--- serverAPI/mix.exs
+++ serverAPI/mix.exs
@@ -33,11 +33,11 @@ defmodule ServerAPI.Mixfile do
     [{:phoenix, "~> 1.2.4"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:mariaex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:sqlite_ecto2, "~> 2.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.

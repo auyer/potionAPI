39a359f84922fff846663c32ef993b0f07b612ce
Auyer <rafa_auyer@icloud.com>
2017-10-05 13:21:27 -0300

    removed debug line

--- serverAPI/lib/storeFile.ex
+++ serverAPI/lib/storeFile.ex
@@ -1,43 +1,44 @@
 defmodule ServerAPI.StoreFile do
 
-  @moduledoc """
-  Responsible for accepting files and uploading them to an asset store.
-  """
+	@moduledoc """
+	Responsible for managing files (Uploading and deletion).
+	"""
 
-  @doc """
-  Accepts a base64 encoded image and saves it
+	@doc """
+	Accepts a base64 encoded image and saves it with a generated name and the provided extension.
 
-  ## Examples
+	## Examples
 
-      iex> upload_image( data_encoded, file_extension)
+			iex> upload( 64b_encoded_data, file_extension)
+
+						Expected output  {:ok, url}
+						Error output     {:error, reason}
+
+	"""
+
+	def upload(base64,ext) do
+		# Decode the image
+		case Base.decode64(base64) do
+		{:ok, binary} ->
+			filename = unique_filename(ext)
+			case File.write("priv/static/files/" <> filename, binary) do
+				:ok -> {:ok, "/files/" <> filename}
+				{:error, reason}-> {:error, reason}
+			end
+		{:error} -> {:error,"Error Converting from Base 64"}
+		end
+	end
 
 
-  """
-
-  def upload(base64,ext) do
-    # Decode the image
-    case Base.decode64(base64) do
-    {:ok, binary} ->
-      filename = unique_filename(ext)
-      IO.inspect(binary)
-      case File.write!("priv/static/files/" <> filename, binary) do
-        :ok -> {:ok, "/files/" <> filename}
-        {:error, reason}-> {:error, reason}
-      end
-    {:error} -> {:error,"Error Converting from Base 64"}
-    end
-  end
-
-
-  # Generates a unique filename with a given extension
-  defp unique_filename(ext) do
-    case ext do
-      {:ok, extension}->
-        UUID.uuid4(:hex) <> "." <> extension
-      _ ->
-        {:error}
-    end
-  end
+	# Generates a unique filename with a given extension
+	defp unique_filename(ext) do
+		case ext do
+			{:ok, extension}->
+				UUID.uuid4(:hex) <> "." <> extension
+			_ ->
+				{:error}
+		end
+	end
 
 
 

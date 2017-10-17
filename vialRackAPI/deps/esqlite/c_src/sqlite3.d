e8354d5c6b9ae87753c26dc3ccca1d33310a9037
Auyer <rafa_auyer@icloud.com>
2017-10-17 10:29:26 -0200

    deleted binaries

--- vialRackAPI/deps/esqlite/c_src/sqlite3.d
+++
@@ -1,2 +0,0 @@
-/Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/vialRackAPI/deps/esqlite/c_src/sqlite3.o: \
-  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/vialRackAPI/deps/esqlite/c_src/sqlite3.c

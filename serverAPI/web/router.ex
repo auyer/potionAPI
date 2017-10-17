1865867c9b2f041fd9b2f85c5bdf656d250655d7
Auyer <rafa_auyer@icloud.com>
2017-10-17 10:02:20 -0200

    modified routes

--- serverAPI/web/router.ex
+++ serverAPI/web/router.ex
@@ -1,31 +1,26 @@
 defmodule ServerAPI.Router do
-  use ServerAPI.Web, :router
+	use ServerAPI.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+	pipeline :browser do
+		plug :accepts, ["html"]
+		plug :fetch_session
+		plug :fetch_flash
+		plug :protect_from_forgery
+		plug :put_secure_browser_headers
+	end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
 
-  scope "/files", ServerAPI do
-    pipe_through :browser # Use the default browser stack
+	pipeline :api do
+		plug :accepts, ["json"]
+	end
 
-  #  get "/", PageController, :index
-  end
+	scope "/api", ServerAPI do
+	 pipe_through :api
 
-   scope "/api", ServerAPI do
-     pipe_through :api
-
-     get "/list", FileController, :list
-     post "/new", FileController, :create
-     get "/file/:id", FileController, :show
-     post "/file/:id", FileController, :update
-     delete "/file/:id", FileController, :delete
-   end
+	 get "/list", FileController, :list
+	 post "/new", FileController, :create
+	 get "/file/:id", FileController, :show
+	 post "/file/:id", FileController, :update
+	 delete "/file/:id", FileController, :delete
+	end
 end

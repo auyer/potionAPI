ad916add01fc026e2bbe43066bc2a5794d10f3b5
Auyer <rafa_auyer@icloud.com>
2017-10-17 10:20:20 -0200

    renamed folder

---
+++ vialRackAPI/deps/esqlite/c_src/esqlite3_nif.d
@@ -0,0 +1,8 @@
+/Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/esqlite3_nif.o: \
+  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/esqlite3_nif.c \
+  /opt/local/lib/erlang/erts-9.0/include/erl_nif.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_drv_nif.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_int_sizes_config.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_nif_api_funcs.h \
+  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/sqlite3.h \
+  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/queue.h
---
+++ vialRackAPI/deps/esqlite/c_src/queue.d
@@ -0,0 +1,7 @@
+/Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/queue.o: \
+  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/queue.c \
+  /Users/Auyer/Documents/IFB_DOCS/Modules/2017/2/Paradigmas/potionAPI/serverAPI/deps/esqlite/c_src/queue.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_nif.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_drv_nif.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_int_sizes_config.h \
+  /opt/local/lib/erlang/erts-9.0/include/erl_nif_api_funcs.h
